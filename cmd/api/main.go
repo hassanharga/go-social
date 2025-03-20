9b75fa38a15966be8e66690ab20e52d186a92de4
HaSsan <hmohamed@innovationteam.com>
2025-03-20 16:29:35 +0200

    Add database connection handling and Docker Compose configuration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"github/hassanharga/go-api/internal/db"
 	"github/hassanharga/go-api/internal/env"
 	"github/hassanharga/go-api/internal/store"
+	"log"
 )
 
 func main() {
-	store := store.NewStorage(nil)
+	config := config{
+		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		}}
+
+	db, err := db.New(config.db.addr, config.db.maxOpenConns, config.db.maxIdleConns, config.db.maxIdleTime)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	log.Println("Connected to the database")
+
+	store := store.NewStorage(db)
+
 	app := &application{
-		config: config{addr: env.GetString("ADDR", ":8080")},
+		config: config,
 		store:  store,
 	}
 
