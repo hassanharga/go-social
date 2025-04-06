4bb282219f989211a3e5e82efec0834a89c7e02b
HaSsan <hmohamed@innovationteam.com>
2025-04-06 13:23:49 +0200

    Implement pagination and filtering for user feed retrieval

--- cmd/api/feeds.go
+++ cmd/api/feeds.go
@@ -1,12 +1,34 @@
 package main
 
-import "net/http"
+import (
+	"github/hassanharga/go-social/internal/store"
+	"github/hassanharga/go-social/utils"
+	"net/http"
+)
 
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 	// pagination,filter
+	fq := store.PaginatedFeedQuery{
+		Limit:  20,
+		Offset: 0,
+		Sort:   "desc",
+	}
+
+	// log.Println("getUserFeedHandler", fq)
+
+	fq, err := fq.Parse(r)
+	if err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
+
+	if err := utils.Validate.Struct(fq); err != nil {
+		app.badRequestError(w, r, err)
+		return
+	}
 
 	ctx := r.Context()
-	feed, err := app.store.Posts.GetFeed(ctx, int64(42))
+	feed, err := app.store.Posts.GetFeed(ctx, int64(82), fq)
 	if err != nil {
 		app.internalServerError(w, r, err)
 		return
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -22,7 +22,7 @@ type Storage struct {
 		GetById(context.Context, int64) (*Post, error)
 		Delete(context.Context, int64) error
 		Update(context.Context, *Post) error
-		GetFeed(context.Context, int64) ([]*PostWithMetadata, error)
+		GetFeed(context.Context, int64, PaginatedFeedQuery) ([]*PostWithMetadata, error)
 	}
 	Comments interface {
 		Create(context.Context, *Comment) error
